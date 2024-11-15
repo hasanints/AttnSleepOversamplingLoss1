cc159b771d6eae87699f87aab3a253fc544d4fb1
hasanints <18320020@std.stei.itb.ac.id>
2024-11-16 02:54:24 +0700

    update

--- model/loss.py
+++ model/loss.py
@@ -2,6 +2,11 @@ import torch
 import torch.nn as nn
 
 
-def weighted_CrossEntropyLoss(output, target, classes_weights, device):
-    cr = nn.CrossEntropyLoss(weight=torch.tensor(classes_weights).to(device))
+# def weighted_CrossEntropyLoss(output, target, classes_weights, device):
+#     cr = nn.CrossEntropyLoss(weight=torch.tensor(classes_weights).to(device))
+#     return cr(output, target)
+
+# Standard CrossEntropyLoss (No Class Weights)
+def CrossEntropyLoss(output, target, device=None):
+    cr = nn.CrossEntropyLoss()  # Standard CrossEntropyLoss without weights
     return cr(output, target)
